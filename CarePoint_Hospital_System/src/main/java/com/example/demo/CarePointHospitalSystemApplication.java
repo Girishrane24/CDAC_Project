a202965847e6047a3727fdd24ff95f1e4e52ff44
Girishrane24 <ranegirish24@gmail.com>
2026-07-04 14:52:41 +0530

    Authentication with user and admin test

--- CarePoint_Hospital_System/src/main/java/com/example/demo/CarePointHospitalSystemApplication.java
+++ CarePoint_Hospital_System/src/main/java/com/example/demo/CarePointHospitalSystemApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.app.UserController", "package com.app.SecurityConfig"})
 public class CarePointHospitalSystemApplication {
 
 	public static void main(String[] args) {
